c87a16aa528fbb2e60bed1607b36387b04cf61f6
Christine Wasike <christinewasike22@gmail.com>
2017-08-17 16:32:58 +0300

    test folder created

---
+++ src/test/java/HeroTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class HeroTest {
+
+    @Test
+    public void Hero_instantiatesCorrectly_true() {
+        Hero myHero = new Hero("Spiderman");
+        assertEquals(true, myHero instanceof Hero);
+    }
+}
