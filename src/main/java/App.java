387fb091469f4c14671ef2cee74967703bcfe59c
Christine Wasike <christinewasike22@gmail.com>
2017-08-19 16:07:52 +0300

    Imported the Map and ArrayList . As well as added code on  displaying a list of heros on the index.html file from information gathered from the form.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,41 @@
+import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
-        String layout = "template/layout.vtl";
+        String layout = "templates/layout.vtl";
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+
+            model.put("heros", request.session().attribute("heros"));
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        post("/heros", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Hero> heros = request.session().attribute("heros");
+            if (heros == null) {
+                heros = new ArrayList<Hero>();
+                request.session().attribute("heros", heros);
+            }
+
+            String heron = request.queryParams("heroname");
+            int heroa = Integer.parseInt(request.queryParams("heroage"));
+            String herop = request.queryParams("heropower");
+            String herow = request.queryParams("heroweakness");
+            Hero newHero = new Hero(heron, heroa, herop, herow);
+            heros.add(newHero); //instance of a hero created here.
+
+            model.put("template", "templates/success.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
     }
 }
