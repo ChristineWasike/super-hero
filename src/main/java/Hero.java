e52f5be0856be44d7c529a527ff214fced05f881
Christine Wasike <christinewasike22@gmail.com>
2017-08-19 16:10:27 +0300

    I have created private  attributes to the Hero constructor such as the name, age, power weakness and whether a hero is on a mission or not. Then rendered this attributes publlic through the getter method so that they ca be accessed by other classes that require them.

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,5 +1,49 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
-    public Hero(String description) {
+    private String mName;
+    private Integer mAge;
+    private String mPower;
+    private String mWeakness;
+    private boolean mMission;
+    private LocalDateTime mCreateAt;
+
+    public Hero(String name, Integer age, String power, String weakness) {
+        mName = name;
+        mAge = age;
+        mPower = power;
+        mWeakness = weakness;
+        mMission = false;
 
     }
+
+    public String getName() {
+        return mName; //creates superhero's name
+    }
+
+    public Integer getAge() {
+        return mAge;  //creates superhero's age
+    }
+
+    public String getPower() {
+        return mPower; //creates superhero's power
+    }
+
+    public String getWeakness() {
+        return mWeakness;
+    }
+
+    public boolean getMission() {
+        return mMission;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return mCreateAt;
+    }
 }
+
+
+
+
