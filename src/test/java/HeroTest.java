9efcef87f2c47fb2016729e8af3ac5ec75a030cd
Christine Wasike <christinewasike22@gmail.com>
2017-08-19 16:18:35 +0300

    Here I have a couple of the tests I carried out to confirm that my code works as it should. I will just list a couple of the behaviours that the tests took care of. Instantiting a hero, instantiating a hero with a name, their age, power, weakness and whether or not they are on a mission. For now the boolean mission reads false, will soon be working with it.

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,11 +1,53 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
+
 
 public class HeroTest {
 
-    @Test
+    @Test //test for hero instance
     public void Hero_instantiatesCorrectly_true() {
-        Hero myHero = new Hero("Spiderman");
+        Hero myHero = new Hero("Spiderman", 35, "spiderwebs", "kryptonite" );
         assertEquals(true, myHero instanceof Hero);
     }
+
+    @Test //name instantiates too
+    public void Hero_instantiatesWithName_String() {
+        Hero myHero = new Hero("Spiderman", 35, "spiderwebs", "kryptonite" );
+        assertEquals("Spiderman", myHero.getName());
+    }
+
+    @Test  //age instatniates too
+
+    public void getAge_instantiatesWithAge_35() {
+        Hero myHero = new Hero("Spiderman", 35, "spiderwebs", "kryptonite");
+        Integer expected = 35;
+        assertEquals(expected, myHero.getAge());
+    }
+
+    @Test //power instantiates too
+    public void getPower_instantiatesWithPower_String() {
+        Hero myHero = new Hero("Spiderman", 35, "spiderwebs", "kryptonite");
+        assertEquals("spiderweb", myHero.getPower());
+    }
+
+    @Test //weakness instantiates too
+    public void getWeakness_instantiatesWithWeakness_String() {
+        Hero myHero = new Hero("Spiderman", 35, "spiderwebs", "kryptonite");
+        assertEquals("kryptonite", myHero.getWeakness());
+    }
+
+    @Test //hero on mission
+    public void getMission_isFalseAfterInstantiation_false() {
+        Hero myHero = new Hero("Spiderman", 35, "spiderwebs", "kryptonite");
+        assertEquals(false, myHero.getMission());
+    }
+
+    @Test //time of creating hero
+    public void getCreatedAt_instantiesWithCurrentTime_today() {
+        Hero myHero = new Hero("Spiderman", 35, "spiderwebs", "kryptonite");
+        assertEquals(LocalDateTime.now().getDayOfWeek(), myHero.getCreateAt().getDayOfWeek());
+    }
 }
+
+
